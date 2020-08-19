a23ed2b58d13d70d04f2f6929347ec0c40dec166
Bupt-Wangfang <1654944902@qq.com>
2020-08-19 14:46:11 +0800

    update name of controller and service

--- server-app/app/controller/article.js
+++ server-app/app/controller/article.js
@@ -5,7 +5,7 @@ const Controller = require('egg').Controller;
 class ArticleController extends Controller {
   async showAllArticle() { // 异步请求防止阻塞
     const { ctx } = this;
-    const result = await ctx.service.articleService.getAllService();
+    const result = await ctx.service.article.getAllArticle();
     ctx.body = {
         code: 200,
         msg: result
