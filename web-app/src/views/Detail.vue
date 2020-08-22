f2611f733a46d22cba73357e2175a32fa394186a
Bupt-Wangfang <1654944902@qq.com>
2020-08-22 10:17:42 +0800

    modify detail page

--- web-app/src/views/Detail.vue
+++ web-app/src/views/Detail.vue
@@ -1,13 +1,31 @@
 <template>
-    <div></div>
+    <div>
+        <van-panel :title="detail.title">
+            <div>{{ detail.content }}</div>
+            <template #footer>{{detail.createTime}}</template>
+        </van-panel>
+    </div>
 </template>
 
 <script>
+import { Panel } from 'vant';
+import moment from "moment";
+
 export default {
-    components:{},
+    components:{
+        [Panel.name]: Panel
+    },
     data(){
         return {
+            detail: {}
         }
+    },
+    created() {
+        this.$axios.get("http://localhost:7001/article/" + this.$route.query.id)
+        .then(res => {
+            res.data.msg.createTime = moment(res.data.msg.createTime).format("YYYY-MM-DD HH:mm:ss");
+            this.detail = res.data.msg;
+        });
     },
     methods:{},
 }
